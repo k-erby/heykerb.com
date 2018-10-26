e05584a058ae9bd347d51e9d729dde671c8dc281
Kaitlin <cmykaitlin@gmail.com>
2018-10-25 23:00:20 -0700

    Add v basic media query and animations

--- app/js/testimonials.js
+++ app/js/testimonials.js
@@ -16,8 +16,8 @@ testimonials = [
       quote:  'You are an amazing teacher and an even better person (which is saying a lot considering you are a pretty goddamn fabulous teacher).',
       person: 'Previous Student'
     }, {
-      quote: 'I hope to be as inspiring and amazing as you one day.',
-      person: 'Previous Student'
+      quote: 'Kaitlin === best, no type coercion',
+      person: 'Chris Pouliot'
     }, {
       quote:  'Kaitlin\'s willingness to accept constructive criticism and to work towards the high standards she sets for herself have resulted in signifigant growth.',
       person: 'Midway practicum report, Regan Rasmussen & Moira Raynor-Atterbury'
@@ -25,8 +25,8 @@ testimonials = [
       quote:  'Kaitlin\'s positive, caring nature; strong organizational skills; exemplary work ethic; ... and, her broad knowledge base, particulary in the visual arts have resulted in an outstanding practicum.',
       person: 'Final practicum report, Regan Rasmussen, Moira Raynor-Atterbury & Meghan McMillan'
     }, {
-      quote:  'There should be more you\'s in the world',
-      person: 'Jonathan Bezeau'
+      quote:  'Meow >:(',
+      person: 'My cat'
     }
   ];
 
@@ -56,7 +56,6 @@ function onGetTestimonialClick(e) {
 /*
 * on page ready
 */
-document.addEventListener("DOMContentLoaded", function() {
-    // fire on get testimonal click to get the first testimonial
+document.addEventListener("DOMContentLoaded", () => {
     onGetTestimonialClick();
 });
